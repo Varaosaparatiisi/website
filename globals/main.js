f284549b25c2bf6d0555f4a8792a455f496a7e73
Pertti Martikainen <pertti@logistor.com>
2024-09-10 09:40:20 +0000

    header changes when scrolled

--- globals/main.js
+++ globals/main.js
@@ -1,8 +1,12 @@
 window.addEventListener("scroll", function() {
   var header = document.querySelector("header");
+  var logo = document.querySelector(".logo");
+
   if (window.scrollY > 50) {
       header.classList.add("scrolled");
+      logo.src = "/img/logo-black.png"
   } else {
       header.classList.remove("scrolled");
+      logo.src = "/img/logo-white.png"
   }
 });
